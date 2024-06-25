07bfab811c8305f3c6eb8f604cc6a319521d0b74
Brayden Harris <brayden4012@gmail.com>
2024-06-25 14:04:50 -0500

    Initial Commit

---
+++ APOD/APODApp.swift
@@ -0,0 +1,17 @@
+//
+//  APODApp.swift
+//  APOD
+//
+//  Created by Brayden Harris on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct APODApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
